f4eef9c2785f3fbaf8b703cd6a59dd5581d9e5cb
unknown <adarshaadarsha326@gmail.com>
2025-07-27 22:58:59 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/cloud_Shop/", // 👈 very important!
+  base: "/Cloudshop/", // 👈 very important!
   plugins: [react()],
 });
